9b036b0393fc73afed52229debfdac5959955753
Marcelo Silva <marcelo.dev.js@hotmail.com>
2022-08-11 23:11:50 -0300

    refactor: routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,11 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { Home } from '../pages';
+import { Home, Login } from '../pages';
 
 
 export const AppRoutes = () => {
 
 	return (
 		<Routes>
-			<Route path="/" element={<Home />} />
 			<Route path="/home" element={<Home />} />
 			<Route path="*" element={<Navigate to="/home" />} />
 		</Routes>
